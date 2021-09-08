90e9cbac14054540ac0b73fe12d4537572b05a4c
RogerioDC04 <dominguescanilharogerio@gmail.com>
2021-09-08 13:36:43 -0300

    RogerioDC04

---
+++ EXERCICIOS/04_REACT/semana-02/gerenciador-de-tarefas/src/components/App.jsx
@@ -0,0 +1,15 @@
+import './app.css';
+import ExternalCard from '../external-card/ExternalCard.jsx';
+
+function App() {
+  return (
+    <main>
+      <div className="container">
+        <ExternalCard>Minhas Tarefas</ExternalCard>
+
+      </div>
+    </main>
+  );
+}
+
+export default App;
